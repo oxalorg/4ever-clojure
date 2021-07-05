99bcff2c0caa24163bd5ee62f1050527fdf1d4ef
Mitesh Shah <mitesh@miteshshah.com>
2021-07-05 22:34:19 +0530

    Make header common to all pages

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -4,8 +4,21 @@
             [app.routes :as routes]
             [reagent.dom :as rdom]))
 
+(defn header []
+  [:header
+   [:h1 "4ever-clojure"]
+   [:p
+    [:small
+     [:a {:href "https://github.com/oxalorg/4ever-clojure"} "github"]
+     " | "
+     "Built with ❤ by "
+     [:a {:href "https://twitter.com/oxalorg"} "@oxalorg"]
+     " and "
+     [:a {:href "https://twitter.com/lambdaisland"} "@lambdaisland"]]]])
+
 (defn main []
   [:div
+   [header]
    (if-let [match @routes/match]
      (let [view (:view (:data match))]
        [view match]))])
--- src/app/home.cljs
+++ src/app/home.cljs
@@ -2,29 +2,11 @@
   (:require [app.data :as data]
             [app.state :as state]))
 
-(defn header []
-  [:header
-   [:h1 "4ever-clojure"]
-   [:p
-    "Keeping 4clojure alive forever! This website is completely static and evals
-     code using sci."]
-   [:p
-    "If people start using this we can think about adding a simple ranking
-     system using something like firebase."]
-   [:p
-    [:small
-     "Built with ❤ by "
-     [:a {:href "https://twitter.com/oxalorg"} "@oxalorg"]
-     " and "
-     [:a {:href "https://twitter.com/lambdaisland"} "@lambdaisland"]]]]
-  )
-
 (defn problem-list-item [{:keys [_id title]}]
   [:li
    ^{:key _id}
    [:a {:href (state/href :problem/item {:id _id})}
-    title]]
-  )
+    title]])
 
 (defn problem-list []
   [:<>
@@ -35,6 +17,11 @@
 
 (defn view []
   [:div
-   [header]
+   [:p
+    "Keeping 4clojure alive forever! This website is completely static and evals
+     code using sci."]
+   [:p
+    "If people start using this we can think about adding a simple ranking
+     system using something like firebase."]
    [problem-list]]
   )
