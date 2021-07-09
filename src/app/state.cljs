99ef26d31fbb1a5f3bf2a168b2b37092976e487b
Mitesh Shah <mitesh@miteshshah.com>
2021-07-10 04:13:59 +0530

    clean up reagent data flow

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -1,8 +1,22 @@
 (ns app.state
-  (:require [reitit.frontend :as rf]
+  (:require [reagent.core :as r]
+            [alandipert.storage-atom :as lstore]
+            [reitit.frontend :as rf]
             [reitit.frontend.controllers :as rfc]
             [reitit.frontend.easy :as rfe]))
 
 (def navigate rfe/push-state)
 
 (def href rfe/href)
+
+(def default-db
+  "default db example:
+    {:solutions {12 {:code \"(fn [x] (apply + x))\"
+                     :passed 2
+                     :failed 3}
+                 44 {...}}}"
+  {:solutions {}})
+
+(defonce db 
+  (lstore/local-storage (r/atom {})
+                        :4ever-clojure))
