e77b281392a091472c03b717cd15d1b1ecabfed0
Martynas M <martynas.maciulevicius@pm.me>
2023-01-22 18:14:26 +0200

    Fix modal size

--- src/app/modal.cljs
+++ src/app/modal.cljs
@@ -2,19 +2,20 @@
 
 (def modal-style {:position "fixed",
                   :width "700px"
+                  :max-width "100%"
                   :top "50%",
                   :left "50%",
                   :transform "translate(-50%, -50%)",
                   :background "white",
                   :borderRadius "5px",
                   :boxShadow "0 0 0 100vw rgba(0, 0, 0, 0.5)",
-                  :padding "0px 30px 20px 30px",
                   :zIndex "9999"})
 
 (defn box [{:keys [is-open on-close]} & children]
   (when @is-open
     [:div {:style modal-style}
-     children
-     [:button {:on-click on-close} "Close"]]))
+     [:div {:style {:padding "0px 30px 20px 30px"}}
+      children
+      [:button {:on-click on-close} "Close"]]]))
 
 
