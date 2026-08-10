83f32fedbf05d1160732343d67e786b8201851a4
Patrick Barton Grace <132808103+pbgcommits@users.noreply.github.com>
2026-08-11 00:06:37 +1000

    Remove spec usage, change to try-catch

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -1,7 +1,6 @@
 (ns app.state
   (:require [alandipert.storage-atom :as lstore]
             [cljs.reader :refer [read-string]]
-            [cljs.spec.alpha :as spec]
             [reagent.core :as r]
             [reitit.frontend.easy :as rfe]))
 
@@ -23,14 +22,6 @@
   (lstore/local-storage (r/atom {})
                         :4ever-clojure))
 
-;; E.g. {1 {:code "true", :passed 1, :failed 0}, 19 {:code "(comp first reverse)", :passed 3, :failed 0}}
-(spec/def ::code string?)
-(spec/def ::passed number?)
-(spec/def ::failed number?)
-(spec/def ::solution
-  (spec/keys :req-un [::code ::passed ::failed]))
-(spec/def ::solutions
-  (spec/map-of number? ::solution))
 
 (defn new-raw-html-el
   [tag attrs]
@@ -38,9 +29,22 @@
    (js/document.createElement tag)
    (clj->js attrs)))
 
+;; E.g. {1 {:code "true", :passed 1, :failed 0}, 19 {:code "(comp first reverse)", :passed 3, :failed 0}}
 (defn validate-solution-data
+  "Returns the data if valid. Throws an exception otherwise."
   [data]
-  (when (spec/valid? ::solutions data) data))
+  (if
+   (and
+    (map? data)
+    (every?
+     (fn [[id {:keys [code passed failed]}]]
+       (and
+        (number? id)
+        (string? code)
+        (number? passed)
+        (number? failed))) data))
+    data
+    (throw (js/Error. "Invalid data format"))))
 
 (defn import-user-data
   "Import user data from a .edn file"
@@ -52,11 +56,11 @@
                           reader (new js/FileReader)]
                       (set!
                        (.-onload reader)
-                       #(if-let [result (-> (.-result reader)
-                                            (read-string)
-                                            (validate-solution-data))]
-                          (reset! (r/cursor db [:solutions]) result)
-                          (js/alert "Invalid data format")))
+                       #(try (->> (.-result reader)
+                                  (read-string)
+                                  (validate-solution-data)
+                                  (reset! (r/cursor db [:solutions])))
+                             (catch :default e (js/alert (str "Invalid data: " (.-message e))))))
                       (set! (.-onerror reader) #(js/alert "Error reading file"))
                       (. reader readAsText data)))]
     (. upload addEventListener "change" on-upload)
